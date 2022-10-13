39db0761af805879d9d91ad168652487a3ddb285
Cristiano Alves Pereir <cristianomaster77@gmail.com>
2022-10-13 11:29:30 -0300

    Novos dados inesrido

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.lang.invoke.ConstantCallSite;
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas turma DIO");
+        Scanner entrada = new Scanner(System.in);
+       System.out.println("Digite seu primeironome : ");
+        String primeiroNome= entrada.nextLine();
+        System.out.println("Digite seu segundo nome : " );
+        String segundoNome= entrada.nextLine();
+        String nomeCompleto= nomeCompleto(primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+       
+    }
+    public static String nomeCompleto (String primeiroNome, String segundonome){
+        return "Resultado do metodo é " + primeiroNome.concat("").concat(segundonome);
     }
 }
